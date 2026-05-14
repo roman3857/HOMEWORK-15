8e6a19126959331011630403516a04ebcfbf506b
roman3857 <romanshakhrai6@gmail.com>
2026-05-14 18:56:27 +0200

    сщььше

--- script.js
+++ script.js
@@ -1,206 +1,202 @@
 const users = [
-
   {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
 
-    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+    name: "Moore Hensley",
 
-    name: 'Moore Hensley',
+    email: "moorehensley@indexia.com",
 
-    email: 'moorehensley@indexia.com',
+    eyeColor: "blue",
 
-    eyeColor: 'blue',
-
-    friends: ['Sharron Pace'],
+    friends: ["Sharron Pace"],
 
     isActive: false,
 
     balance: 2811,
 
-    skills: ['ipsum', 'lorem'],
+    skills: ["ipsum", "lorem"],
 
-    gender: 'male',
+    gender: "male",
 
     age: 37,
-
   },
 
   {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
 
-    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+    name: "Sharlene Bush",
 
-    name: 'Sharlene Bush',
+    email: "sharlenebush@tubesys.com",
 
-    email: 'sharlenebush@tubesys.com',
+    eyeColor: "blue",
 
-    eyeColor: 'blue',
-
-    friends: ['Briana Decker', 'Sharron Pace'],
+    friends: ["Briana Decker", "Sharron Pace"],
 
     isActive: true,
 
     balance: 3821,
 
-    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
 
-    gender: 'female',
+    gender: "female",
 
     age: 34,
-
   },
 
   {
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
 
-    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: "Ross Vazquez",
 
-    name: 'Ross Vazquez',
+    email: "rossvazquez@xinware.com",
 
-    email: 'rossvazquez@xinware.com',
+    eyeColor: "green",
 
-    eyeColor: 'green',
-
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
 
     isActive: false,
 
     balance: 3793,
 
-    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
 
-    gender: 'male',
+    gender: "male",
 
     age: 24,
-
   },
 
   {
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
 
-    id: '249b6175-5c30-44c6-b154-f120923736f5',
+    name: "Elma Head",
 
-    name: 'Elma Head',
+    email: "elmahead@omatom.com",
 
-    email: 'elmahead@omatom.com',
+    eyeColor: "green",
 
-    eyeColor: 'green',
-
-    friends: ['Goldie Gentry', 'Aisha Tran'],
+    friends: ["Goldie Gentry", "Aisha Tran"],
 
     isActive: true,
 
     balance: 2278,
 
-    skills: ['adipisicing', 'irure', 'velit'],
+    skills: ["adipisicing", "irure", "velit"],
 
-    gender: 'female',
+    gender: "female",
 
     age: 21,
-
   },
 
   {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
 
-    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+    name: "Carey Barr",
 
-    name: 'Carey Barr',
+    email: "careybarr@nurali.com",
 
-    email: 'careybarr@nurali.com',
+    eyeColor: "blue",
 
-    eyeColor: 'blue',
-
-    friends: ['Jordan Sampson', 'Eddie Strong'],
+    friends: ["Jordan Sampson", "Eddie Strong"],
 
     isActive: true,
 
     balance: 3951,
 
-    skills: ['ex', 'culpa', 'nostrud'],
+    skills: ["ex", "culpa", "nostrud"],
 
-    gender: 'male',
+    gender: "male",
 
     age: 27,
-
   },
 
   {
+    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
 
-    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: "Blackburn Dotson",
 
-    name: 'Blackburn Dotson',
+    email: "blackburndotson@furnigeer.com",
 
-    email: 'blackburndotson@furnigeer.com',
+    eyeColor: "brown",
 
-    eyeColor: 'brown',
-
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
 
     isActive: false,
 
     balance: 1498,
 
-    skills: ['non', 'amet', 'ipsum'],
+    skills: ["non", "amet", "ipsum"],
 
-    gender: 'male',
+    gender: "male",
 
     age: 38,
-
   },
 
   {
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
 
-    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+    name: "Sheree Anthony",
 
-    name: 'Sheree Anthony',
+    email: "shereeanthony@kog.com",
 
-    email: 'shereeanthony@kog.com',
+    eyeColor: "brown",
 
-    eyeColor: 'brown',
-
-    friends: ['Goldie Gentry', 'Briana Decker'],
+    friends: ["Goldie Gentry", "Briana Decker"],
 
     isActive: true,
 
     balance: 2764,
 
-    skills: ['lorem', 'veniam', 'culpa'],
+    skills: ["lorem", "veniam", "culpa"],
 
-    gender: 'female',
+    gender: "female",
 
     age: 39,
-
   },
-
 ];
 
-
-
-const calculateTotalBalance = users => {
-return users.reduce((totalBankSum, balance) => totalBankSum + balance.balance, 0)
+const calculateTotalBalance = (users) => {
+  return users.reduce(
+    (totalBankSum, balance) => totalBankSum + balance.balance,
+    0,
+  );
 };
 
 console.log(calculateTotalBalance(users)); // 20916
 
-
 const getUsersWithFriend = (users, friendName) => {
+  return users
 
+    .filter((user) => user.friends.includes(friendName))
+    .map((users) => users.name);
 };
 
-console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
 
-
-
-
-const getNamesSortedByFriendsCount = users => {
-
+const getNamesSortedByFriendsCount = (users) => {
+  return [...users]
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map((user) => user.name);
 };
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
 
+const getSortedUniqueSkills = (users) => {
+  return users
+    .reduce((allSkills, user) => {
+      user.skills.forEach(skill => {
+        if (!allSkills.includes(skill)) {
+          allSkills.push(skill);
+        }
+      });
 
-const getSortedUniqueSkills = users => {
-  users.reduce(())
+      return allSkills;
+    }, [])
+    .sort();
 };
+
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
